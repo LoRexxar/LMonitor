7c2a8a51f345c9191525b56f9887ad62568c2ea2
ubuntu <ubuntu@localhost.localdomain>
2023-05-27 02:47:27 +0800

    update

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 while :
 do
     if [ $(ps aux | grep LBotCoreBackend|grep -v grep|wc -l) -eq 0 ];then
         echo "start"
-        /usr/bin/python3 /root/manage.py LBotCoreBackend
+        /usr/bin/python3 /home/ubuntu/LMonitor/manage.py LBotCoreBackend
     fi
     sleep 100
 done
