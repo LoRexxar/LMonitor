1aeb5e7e06faaf1336eb5662415ca76263bc27bf
ubuntu <ubuntu@localhost.localdomain>
2023-05-29 18:23:18 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 while :
@@ -9,16 +8,3 @@ do
     fi
     sleep 100
 done
-=======
-#!/bin/bash
-
-
-while :
-do
-    if [ $(ps aux | grep LMonitorCoreBackend|grep -v grep|wc -l) -eq 0 ];then
-        echo "start"
-        /usr/bin/python3 /root/manage.py LMonitorCoreBackend
-    fi
-    sleep 100
-done
->>>>>>> e7cc5a8193a073e6a0097de29aa07fc742355dd7
