07f0329dcc2583fa510823e1130b0a0ab1dd5304
LoRexxar <lorexxar@gmail.com>
2025-08-05 17:31:07 +0800

    update wechatMonitor

--- botend/controller/plugins/info/wechatMonitor.py
+++ botend/controller/plugins/info/wechatMonitor.py
@@ -94,28 +94,32 @@ class WechatMonitor(BaseScan):
                 return
 
             r = json.loads(content)
-            if 'app_msg_list' in r:
-                for msg in r['app_msg_list']:
-                    cover = msg['cover']
-                    create_time = datetime.datetime.fromtimestamp(msg['create_time'])
-                    digest = msg['digest']
-                    link = msg['link']
-                    title = msg['title']
+            if 'publish_page' in r:
+                content_full = json.loads(r['publish_page'])
+                if "publish_list" in content_full:
+                    for msg in content_full['publish_list']:
+                        article_content = json.loads(msg["publish_info"])
+                        article_data = article_content["data"]
+                        cover = article_data['cover']
+                        create_time = datetime.datetime.fromtimestamp(article_data['create_time'])
+                        digest = article_data['digest']
+                        link = article_data['link']
+                        title = article_data['title']
 
-                    parsed_url = urlparse(link)
-                    query_params = parse_qs(parsed_url.query)
-                    sn = query_params.get('sn')[0]
+                        # parsed_url = urlparse(link)
+                        # query_params = parse_qs(parsed_url.query)
+                        # sn = query_params.get('sn')[0]
 
-                    waa = WechatArticle.objects.filter(sn=sn).first()
+                        waa = WechatArticle.objects.filter(link=link).first()
 
-                    if waa:
-                        continue
+                        if waa:
+                            continue
 
-                    obj = WechatArticle(title=title, url=link, publish_time=create_time,
-                                        biz=wat.biz, digest=digest, cover=cover,
-                                        sn=sn, state=0)
-                    obj.save()
-                    logger.info("[Wechat Monitor] Found new Wechat article.")
+                        obj = WechatArticle(title=title, url=link, publish_time=create_time,
+                                            biz=wat.biz, digest=digest, cover=cover,
+                                            sn="", state=0)
+                        obj.save()
+                        logger.info("[Wechat Monitor] Found new Wechat article.")
 
             time.sleep(random.randint(120, 300))
 
