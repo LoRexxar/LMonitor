bdb156471cbe8164180e37dc5b1fe6521899af2c
LoRexxar <lorexxar@users.noreply.github.com>
2026-06-12 10:20:08 +0800

    fix(deploy): specify remote branch, remove redundant merge step

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,17 @@
 set -e
 
 echo "=== 1. Git pull ==="
-git pull
+GIT_MERGE_AUTOEDIT=no git pull origin master
 
-echo "=== 2. Git merge ==="
-# 自动接受默认 merge message，不打开 vim
-GIT_MERGE_AUTOEDIT=no git merge origin/master
-
-echo "=== 3. Collectstatic ==="
-# 自动回答 yes 覆盖
+echo "=== 2. Collectstatic ==="
 yes | python3 manage.py collectstatic
 
-echo "=== 4. 重启 lmweb ==="
-# 发 Ctrl+C 杀掉旧进程，再启动
+echo "=== 3. 重启 lmweb ==="
 screen -S lmweb -X stuff $'\cc'
 sleep 2
 screen -S lmweb -X stuff $'python3 manage.py runserver 0.0.0.0:1800\n'
 
-echo "=== 5. 重启 lmback ==="
+echo "=== 4. 重启 lmback ==="
 screen -S lmback -X stuff $'\cc'
 sleep 2
 screen -S lmback -X stuff $'./start.sh\n'
