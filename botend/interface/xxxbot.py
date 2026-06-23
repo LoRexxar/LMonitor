7e3ebd639bfdbeb8c502ab5de2fa0596c361f5ce
LoRexxar <lorexxar@users.noreply.github.com>
2026-06-23 16:32:24 +0800

    fix: disable xxxbot calls by default

--- botend/interface/xxxbot.py
+++ botend/interface/xxxbot.py
@@ -22,10 +22,20 @@ class xxxbotInterface:
     """
     def __init__(self):
         self.config = getattr(django_settings, 'XXXBOT_CONFIG', {}) or {}
+        self.enabled = bool(self.config.get("enabled", False))
         self.base_url = self.config.get("base_url", "")
         self.active_roomlist = self.config.get("active_roomlist", [])
         self.wxid = self.config.get("wxid", "")
         self.s = requests.Session()
+
+    def _is_enabled(self):
+        if not self.enabled:
+            logger.info("xxxbot disabled, skip message send")
+            return False
+        if not self.base_url or not self.wxid:
+            logger.warning("xxxbot config incomplete, skip message send")
+            return False
+        return True
 
     def send_msg(self, content="", at_str=""):
         """
@@ -33,6 +43,9 @@ class xxxbotInterface:
         :param msg:
         :return:
         """
+        if not self._is_enabled():
+            return
+
         url = "{}/VXAPI/Msg/SendTxt".format(self.base_url)
 
         for room in self.active_roomlist:
@@ -54,6 +67,9 @@ class xxxbotInterface:
                 logger.error("send msg to {} failed: {}".format(room, e))
 
     def publish_admin(self, content="", at_str=""):
+        if not self._is_enabled():
+            return
+
         url = "{}/VXAPI/Msg/SendTxt".format(self.base_url)
         data = {
                 "Wxid": self.wxid, 
